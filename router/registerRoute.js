1f3725d06c4b3b5ec46fa5c7dc690dd32af06174
natheDev <tuanjunior98@gmail.com>
2023-05-29 17:57:38 +0700

    beware of unexpected changes :"

---
+++ router/registerRoute.js
@@ -0,0 +1,15 @@
+
+const router = require('express').Router();
+
+// Ambil index.js dari controller dan panggil variabel didalamnya
+const registerController = require('../controllers').register;
+// Definisikan middleware verify.js
+const verifyUser = require('../middleware/verify.js');
+
+// Rute 'http://localhost:5050/register/' digunakan untuk menampilkan form register
+router.get('/', verifyUser.isLogout, registerController.formRegister);
+// Rute 'http://localhost:5050/register/save' digunakan untuk menyimpan data yang diinput user saat register
+router.post('/save', verifyUser.isLogout, registerController.saveRegister);
+
+
+module.exports = router;
